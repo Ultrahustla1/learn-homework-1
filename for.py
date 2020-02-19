2ba606b10fa8c12b5b94c91740702dbaaf199ec0
Alexander Chernenko <cher.aleksandr@gmail.com>
2020-02-19 23:38:55 +0300

    Дополнил дз

--- for.py
+++ for.py
@@ -10,12 +10,19 @@
 * Посчитать и вывести средний балл по каждому классу.
 """
 
+students_scores = [
+  {'school_class': '4a', 'scores': [3,4,4,5,2]}, 
+  {'school_class': '4b', 'scores': [5,5,3,5,4]}
+]
+
+students_average_score = []
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
-if __name__ == "__main__":
-    main()
+  for s in students_scores:
+      students_average_score.append(sum(s['scores'])/len(s['scores']))
+      print(f"Средняя оценка для класса {s['school_class']} : {(sum(s['scores']) / len(s['scores']))}")
+
+  school_average_score = sum(students_average_score)/len(students_average_score)
+  print(f"Средняя оценка по школе: {school_average_score}")
+  
+main()
